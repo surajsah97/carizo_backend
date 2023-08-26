5bd4ebed96e8fe5499ce1b63c81af7368801a69a
surajsah97 <surajsah059@gmail.com>
2023-08-26 11:16:38 -0700

    update in add to cart

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,111 +1,117 @@
-const Cart = require("../database/model/Cart")
-const Item = require("../database/model/Item")
-const ItemDetail = require("../database/model/ItemDetail")
-const Order = require("../database/model/Order")
-const tax = require("../database/model/tax")
-const AddCart = require("../helper/AddCart")
-const addOrder = require("../helper/addOrder")
+const Cart = require("../database/model/Cart");
+const Item = require("../database/model/Item");
+const ItemDetail = require("../database/model/ItemDetail");
+const Order = require("../database/model/Order");
+const tax = require("../database/model/tax");
+const AddCart = require("../helper/AddCart");
+const addOrder = require("../helper/addOrder");
+let common=require("../helper/commonFunction")
 
-module.exports={
-    getOrder:async(req,res)=>{
-        try{
-            let order=await Cart.find({cartStatus:1})
-            if(order.length>0){
-                return res.send({status:1,message:"success",data:order})
-            }
-            else{
-                return res.send({status:2,message:"something went wrong",data:[]})
-            }
-        }
-        catch(err){
-            return res.send({status:3,message:"something went wrong",data:[]})
-        }
-    },
-    editOrder:async(req,res)=>{
-        try{
-
-        }
-        catch(err){
-            
-        }
-    },
-    deleteOrder:async(req,res)=>{
-        try{
-
-        }
-        catch(err){
-            
-        }
-    
-    },
-    addCart:async(req,res)=>{
-        try{
-            let itemId=req.body.itemId;
-            let quantity=req.body.quantity;
-            let cartId=req.body.cartId;
-            let orderId=req.body.orderId;
-            let userData=req.body.user;
-            let custCart,custOrder;
-            if(!orderId){
-                 custOrder=await Order.findOne({userId:req.body.user.id,stage:1})
-                if(!custOrder){
-                    custOrder=addOrder(userData)
-                }
-                
-            }
-            if(!itemId){
-                return res.send({status:2,message:"item id required"})
-            }
-            if(!cartId){
-                //  custCart=await Cart.findOne({cartId});
-                // if(custCart){
-                //    custCart=await Cart.findOne({itemId,stage:1});
-                // }
-                // else{
-                    
-                custCart=await Cart.findOne({userId:req.body.user.id,itemId,stage:1});
-                // }
-                if(!custCart){
-                // custCart=await Cart.findOne({userId:req.body.user.id,itemId,stage:1});
-
-                }
-            }
-            else{
-
-            }
-            let cart=await Cart.find({itemId:itemId,stage:1})
-            if(cart){
-                let order=await Order.find({id:cart.cartId})
-                
-                let cartOrder=orderCartUpdate(cart,order,quantity)
-            }
-            else{
-            let ItemDetails=await ItemDetail.findOne({id:itemId})
-            if(ItemDetails){
-                let tax=await tax.findOne({id:ItemDetails.taxId})
-                ItemDetail["taxAmount"]=tax.taxAmount
-                ItemDetail["stage"]=1
-                ItemDetail["user"]=req.body.user
-                carts["user"]=req.body.user
-                let carts=AddCart(ItemDetail)
-                let cartAdded=await Cart.create(carts)
-                let order=addOrder(carts)
-                let orderAdded=await Order.create(order)
-                if(orderAdded){
-                    return res.send({status:1,message:"success",data:cartAdded})
-                }
-                else{
-                    return res.send({status:2,message:"something went wrong",data:[]})
-                }
-            }
-            else{
-                return res.send({status:2,message:"something went wrong",data:[]})
-            }
-        }
-        }
-        catch(err){
-            return res.send({status:3,message:"something went wrong",err})
-
-        }
+module.exports = {
+  getOrder: async (req, res) => {
+    try {
+      let order = await Cart.find({ cartStatus: 1 });
+      if (order.length > 0) {
+        return res.send({ status: 1, message: "success", data: order });
+      } else {
+        return res.send({
+          status: 2,
+          message: "something went wrong",
+          data: []
+        });
+      }
+    } catch (err) {
+      return res.send({ status: 3, message: "something went wrong", data: [] });
     }
-}
+  },
+  editOrder: async (req, res) => {
+    try {
+    } catch (err) {}
+  },
+  deleteOrder: async (req, res) => {
+    try {
+    } catch (err) {}
+  },
+  addCart: async (req, res) => {
+    try {
+      let itemId = req.body.itemId;
+      let quantity = req.body.quantity;
+      let cartId = req.body.cartId;
+      let orderId = req.body.orderId;
+      let userData = req.body.user;
+      let custCart, custOrder, itemData;
+      if (!itemId && !cartId) {
+        return res.send({ status: 2, message: "item id required" });
+      }
+      if (itemId) {
+        itemData = await Item.find({ itemId });
+      }
+
+
+      if (!orderId) {
+        custOrder = await Order.findOne({ userId: req.body.user.id, stage: 1 });
+        if (!custOrder) {
+          custOrder = addOrder(userData);
+        }
+        if (cartId) {
+          custCart = await Cart.find({ cartId, status: 1 });
+        } else {
+          custCart = await Cart.find({ itemId, status: 1 });
+        }
+        if (!custCart || !(custCart.length > 0)) {
+          custCart = AddCart(itemData);
+        }
+        let allCart=await Cart.find({orderId:custOrder.orderId})
+        
+        let orderCalculation = common.orderCartUpdate(allCart,custCart,custOrder);
+        if(orderCalculation){
+            res.send({status:1,message:"success",data:orderCalculation})
+        }
+      } else {
+        // if(!cartId){
+        //     //  custCart=await Cart.findOne({cartId});
+        //     // if(custCart){
+        //     //    custCart=await Cart.findOne({itemId,stage:1});
+        //     // }
+        //     // else{
+        // }
+      }
+      let cart = await Cart.find({ userId: req.body.user.id, stage: 1 });
+      if (cart) {
+        let order = await Order.find({ id: cart.cartId });
+
+        let cartOrder = orderCartUpdate(cart, order, quantity);
+      } else {
+        let ItemDetails = await ItemDetail.findOne({ id: itemId });
+        if (ItemDetails) {
+          let tax = await tax.findOne({ id: ItemDetails.taxId });
+          ItemDetail["taxAmount"] = tax.taxAmount;
+          ItemDetail["stage"] = 1;
+          ItemDetail["user"] = req.body.user;
+          carts["user"] = req.body.user;
+          let carts = AddCart(ItemDetail);
+          let cartAdded = await Cart.create(carts);
+          let order = addOrder(carts);
+          let orderAdded = await Order.create(order);
+          if (orderAdded) {
+            return res.send({ status: 1, message: "success", data: cartAdded });
+          } else {
+            return res.send({
+              status: 2,
+              message: "something went wrong",
+              data: []
+            });
+          }
+        } else {
+          return res.send({
+            status: 2,
+            message: "something went wrong",
+            data: []
+          });
+        }
+      }
+    } catch (err) {
+      return res.send({ status: 3, message: "something went wrong", err });
+    }
+  }
+};
--- helper/commonFunction.js
+++ helper/commonFunction.js
@@ -1,11 +1,26 @@
-const orderCartUpdate=(data,order,quantity)=>{
-data.quantity=quantity;
-data.subtotal=data.subtotal+data.price*quantity
-data.total=data.subtotal+data.tax
-if(data){
-    order.subTotal=data.subTotal+order.subTotal
-    order.total=data.total+order.total
-    order.taxId=data.taxId
-    order.updatedDate=new Date().valueOf()
+const Order = require("../database/model/Order");
+
+const orderCartUpdate=async(allCart,custCart,custOrder)=>{
+    allCart.map(el=>{
+        custOrder.subTotal+=el.subTotal;
+        custOrder.total+=el.total
+        custOrder.tax+=el.tax
+    })
+    custOrder.updatedDate=new Date().valueOf();
+    let AddOrder=await Order.create(custOrder)
+    let AddCart=await custCart.create(custCart)
+    if(AddCart&&AddOrder){
+        custOrder.cart=allCart
+        return custOrder
+    }
+// data.quantity=quantity;
+// data.subtotal=data.subtotal+data.price*quantity
+// data.total=data.subtotal+data.tax
+// if(data){
+//     order.subTotal=data.subTotal+order.subTotal
+//     order.total=data.total+order.total
+//     order.taxId=data.taxId
+//     order.updatedDate=new Date().valueOf()
+// }
 }
-}
+module.exports={orderCartUpdate}
